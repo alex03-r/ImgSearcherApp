87b94c624442b8d5d39e90022eedacc913190412
Arodriguez <anyelrodriguezrivera40@gmail.com>
2022-10-29 18:14:36 -0400

    add the key props

--- src/ImageItem.jsx
+++ src/ImageItem.jsx
@@ -10,9 +10,10 @@ export const ImageItem = (props) => {
 const { urls , alt_description, handleImgClick } = props;
 
   return (  
+    
     <div style={{position:"relative", width:"270px" , marginRight:"8px"}} className="d-flex" >
        <img className='img animate__animated animate__bounce'  onMouseOver={() => settooltip(true) } onMouseLeave={() => settooltip(false) }  onClick={() => handleImgClick(urls.raw)} src={urls.raw} alt={alt_description}   />
        { tooltip ? <ToolTip describtion={alt_description} /> : null } 
-    </div>
+    </div>    
     )
 }
--- src/ImgSearcher.jsx
+++ src/ImgSearcher.jsx
@@ -1,16 +1,15 @@
-import React, {  useEffect, useRef, useState } from 'react'
- import { ImageItem } from './ImageItem';
-import './style.css' 
-// const ImageItem = React.lazy(() => import('./ImageItem') )
+import React, { useEffect, useRef, useState  } from 'react'
+import { ImageItem } from './ImageItem';
+import './style.css'
+
+
 
 
 export const ImgSearcher = () => {
 
   const APY_KEY = 'd117mhkj6exY-z_CMSbgOx66rzvd_NPSjsoR6Yx9A6c';
   const [images, setImages] = useState([]);
-  const [removeAlert, setRemoveAlert] = useState(false);
   const [searchImg, setSearchImg] = useState('');
-  // const [noPhothos, setnoPhothos] = useState(true);
 
   const buttonref = useRef("");
 
@@ -27,21 +26,14 @@ export const ImgSearcher = () => {
 
     const data = await response.json();
     const { results } = data;
-
-    setRemoveAlert(false);
-    setImages(results);
     console.log(results)
-
-    // if (images.length <= 0) {
-    //   setnoPhothos(true);
-    // } 
-    //   setnoPhothos(false);
+    setImages(results);
   }
 
   const handleSearch = (e) => {
 
     buttonref.current.removeAttribute("disabled");
-    setSearchImg(e.target.value); 
+    setSearchImg(e.target.value);
 
   }
 
@@ -52,12 +44,11 @@ export const ImgSearcher = () => {
       alert('the input is emty');
       return null;
     }
-    setRemoveAlert(false);
 
     getAllImages();
 
     setSearchImg('');
- 
+
   }
 
   const handleImgClick = (url) => {
@@ -66,42 +57,34 @@ export const ImgSearcher = () => {
 
   useEffect(() => {
     buttonref.current.setAttribute('disabled', 'disabled');
-    setRemoveAlert(true)
-  
  
   }, [])
-
-  // useEffect(()=>{
-
-  //   setnoPhothos( noPhothos => !noPhothos ); 
-  //   console.log(noPhothos)  
-
-  // },[images])
 
 
   return (
     <div>
 
       <h1 className='text-center'>Searcher image App</h1>
-        <div className='d-flex aling-items-center mt-3 ms-5'>
+      <div className='d-flex aling-items-center mt-3 ms-5'>
 
-              <label className='form-label mt-2 ms-5 me-0 fw-bold'>Search your image</label>
-              <input className='form-control mt-2 ms-5 mb-2 w-50' placeholder='Img name' onChange={handleSearch} value={searchImg} />
-              <button className='btn btn-primary h-50 mt-2 ms-2' ref={buttonref} onClick={handleSearchImg}>Search</button>
-        </div>
+        <label className='form-label mt-2 ms-5 me-0 fw-bold'>Search your image</label>
+        <input className='form-control mt-2 ms-5 mb-2 w-50' placeholder='Img name' onChange={handleSearch} value={searchImg} />
+        <button className='btn btn-primary h-50 mt-2 ms-2' ref={buttonref} onClick={handleSearchImg}>Search</button>
+      </div>
+ 
         <div className='row mt-3'>
+          {images.length <= 0 && <p className='alert alert-primary mt-5  text-center'>No phothos yet. Type a keword to search the kind of photos you want   </p> }
 
-          { removeAlert ? <p className='alert alert-primary mt-5  text-center'>No phothos yet. Type a keword to search the kind of photos you want   </p> : null}
+          {
+            images.map(img => {
 
-          {/* { images.length <= 0 && noPhothos  ? <p className=' animate__animated animate__bounce alert alert-info mt-5  text-center'>Sorry no photos found</p> : null} */}
-            {
-              images.map(img => {
-              
-                return  <ImageItem {...img} handleImgClick={ handleImgClick } />
+              return <ImageItem key={img.id}  {...img} handleImgClick={handleImgClick} />
 
-              })
-            }
+            })
+
+          }
         </div>
+      
 
     </div>
   )
